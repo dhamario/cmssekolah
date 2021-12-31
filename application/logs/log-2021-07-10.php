57ab4d2f3b0fc5ecaf788c1427f5462997764aa8
dhamario <dhamario.sch@gmail.com>
2021-12-31 08:29:23 +0700

    cms

---
+++ application/logs/log-2021-07-10.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-07-10 15:23:44 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\cmssekolahku\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-07-10 15:23:44 --> Unable to connect to the database
+ERROR - 2021-07-10 15:25:36 --> 404 Page Not Found: Media_library/images
